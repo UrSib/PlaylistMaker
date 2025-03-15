6a99146c4c2a8c4d12bbce704f5aeb1ad602e540
UrSib <yaroslavtsevandrey24@gmail.com>
2025-03-15 23:46:18 +0700

    Init commit

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +13,37 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val search = findViewById<Button>(R.id.search)
+        val library = findViewById<Button>(R.id.library)
+        val settings = findViewById<Button>(R.id.settings)
+
+        val searchIntent = Intent(this, SearchActivity::class.java)
+
+        val searchClickListener: View.OnClickListener = object : View.OnClickListener {
+
+            override fun onClick(v: View?) {
+
+                startActivity(searchIntent)
+
+            }
+
+        }
+        search.setOnClickListener(searchClickListener)
+
+        library.setOnClickListener {
+
+            val libraryIntent = Intent(this, MediaLibraryActivity::class.java)
+            startActivity(libraryIntent)
+
+        }
+
+        settings.setOnClickListener {
+
+            val settingsIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingsIntent)
+
+        }
 
     }
 }
---
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,25 @@
+package com.practicum.playlistmaker
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        val back = findViewById<LinearLayout>(R.id.back)
+
+        back.setOnClickListener {
+
+            val backIntent = Intent(this, MainActivity::class.java)
+            startActivity(backIntent)
+
+        }
+    }
+}
